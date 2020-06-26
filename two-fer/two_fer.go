47864cb8c4da1676b908c3ac4ce2c99996a118c3
fiplox <56274824+fiplox@users.noreply.github.com>
2020-06-26 22:44:46 +0200

    second iteration

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,9 +1,14 @@
-package twofer
+package twofer // import "twofer"
 
+import "strings"
+
+//ShareWith returns the string with the given name or "you"if name is empty.
 func ShareWith(name string) string {
-	if len(name) == 0 {
-		return "One for you, one for me."
-	} else {
-		return ("One for " + name + ", one for me.")
+	share := "One for you, one for me."
+
+	if name == "" {
+		return share
 	}
+
+	return strings.Replace(share, "you", name, 1)
 }
