506b7fce6d74e8e75512b16a2fb56760ce8f7770
fiplox <56274824+fiplox@users.noreply.github.com>
2020-06-27 09:45:52 +0200

    second iteration

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -3,16 +3,30 @@ package isogram
 
 import "strings"
 
-// IsIsogram verifies if given word/phare is isogram
-func IsIsogram(s string) bool {
-	s = strings.ToUpper(s)
-	var n int
+// Once returns true if a given rune is present once in a string
+func Once(s string, c rune) bool {
+	n := 0
 	for _, ss := range s {
 		if ss == ' ' || ss == '-' {
 			continue
 		}
-		n = strings.Count(s, string(ss))
+		if c == ss {
+			n++
+		}
+
 		if n > 1 {
+			return false
+		}
+	}
+
+	return true
+}
+
+// IsIsogram verifies if given word/phare is isogram
+func IsIsogram(s string) bool {
+	s = strings.ToUpper(s)
+	for _, ss := range s {
+		if !Once(s, ss) {
 			return false
 		}
 	}
