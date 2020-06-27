925484ac81f527d8dce1d2903da6e4f6516bba1c
fiplox <56274824+fiplox@users.noreply.github.com>
2020-06-27 10:00:23 +0200

    second iteration

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,29 +1,21 @@
 package diffsquares
 
-// SquareOfSum returns the square of the sum of first n numbers.
+// SquareOfSum returns the square of the sum of first n numbers
+/* 1 + 2 + 3 + ... + n = n(n + 1) / 2
+   (n(n + 1) / 2)^2 = n^2(n+1)^2 / 4
+*/
 func SquareOfSum(n int) int {
-	var res int
-
-	for i := 0; i <= n; i++ {
-		res += i
-	}
-
-	return res * res
+	return n * n * (n*n + 2*n + 1) / 4
 }
 
-// SumOfSquares returns the sum of the squares of first n numbers.
+// SumOfSquares returns the sum of the squares of first n numbers
+// 1^2 + 2^2 + 3^2 + ... + n^2 = n(n + 1)(2n + 1) / 6
 func SumOfSquares(n int) int {
-	var res int
-
-	for i := 0; i <= n; i++ {
-		res += i * i
-	}
-
-	return res
+	return n * (n + 1) * (2*n + 1) / 6
 }
 
 // Difference returns the difference between
-// square of the sum and the sum of the squares.
+// square of the sum and the sum of the squares
 func Difference(n int) int {
 	return SquareOfSum(n) - SumOfSquares(n)
 }
