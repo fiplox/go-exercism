e24351684ffe3909cc98822a4711f9e39bc5a411
fiplox <56274824+fiplox@users.noreply.github.com>
2020-08-09 16:25:19 +0200

    first iteration

---
+++ tree-building/go.mod
@@ -0,0 +1,5 @@
+module tree
+
+go 1.13
+
+require mvdan.cc/gofumpt v0.0.0-20200802201014-ab5a8192947d // indirect
---
+++ tree-building/tree_building.go
@@ -0,0 +1,48 @@
+package tree
+
+import (
+	"errors"
+	"sort"
+)
+
+type Record struct {
+	ID     int
+	Parent int
+}
+
+type Node struct {
+	ID       int
+	Children []*Node
+}
+
+// Build builds a tree of nodes from given records.
+func Build(records []Record) (*Node, error) {
+	if len(records) == 0 {
+		return nil, nil
+	}
+
+	n := make([]*Node, len(records))
+	sort.SliceStable(records, func(i, j int) bool {
+		return records[i].ID < records[j].ID
+	})
+	for i, r := range records {
+		n[i] = &Node{ID: r.ID}
+		if i == 0 && r.ID != 0 {
+			return nil, errors.New("no root node")
+		} else if i == 0 && r.Parent != 0 {
+			return nil, errors.New("root node has parent")
+		} else if i == 0 {
+			continue
+		} else if i != r.ID {
+			return nil, errors.New("non-continuous")
+		} else if r.ID <= r.Parent {
+			return nil, errors.New("higher id parent of lower id")
+		}
+
+		if n[r.Parent] != nil {
+			n[r.Parent].Children = append(n[r.Parent].Children, n[i])
+		}
+	}
+
+	return n[0], nil
+}
